b430a35a2bee6e81e8bd71f21f2160a176096048
tothdomi22 <tothdomi22@gmail.com>
2025-11-10 19:41:55 +0100

    business validation

--- src/main/java/com/codewithmosh/store/repositories/UserRepository.java
+++ src/main/java/com/codewithmosh/store/repositories/UserRepository.java
@@ -4,4 +4,6 @@ import com.codewithmosh.store.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+  boolean existsByEmail(String email);
+}
